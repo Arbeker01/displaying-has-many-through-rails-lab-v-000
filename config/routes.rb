a82b432431f273913ee978354c70f565880390e8
Romeo Gono <romeogono1@aol.com>
2019-06-26 10:04:04 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
 resources :doctors
 resources :patients
-resources :appointments, only: [:show]
-  get 'index' => 'doctors#index'
+#resources :appointments, only: [:show]
 end
