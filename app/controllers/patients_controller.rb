5384ded3765461d430c12464cd9386b530779d13
Romeo Gono <romeogono1@aol.com>
2019-06-26 10:26:57 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,23 +1,23 @@
 class PatientsController < ApplicationController
 
   def show
-    @doctor = Doctor.find(params[:id])
+    @patient = Patient.find(params[:id])
   end
     def index
-      @doctors = Doctor.all
+      @patients = Patient.all
     end
 
     def new
-      @doctor = Doctor.new
+      @patient = Patient.new
     end
 
     def create
-      doctor = Doctor.create(doctor_params)
-      redirect_to doctor
+      patient = Patient.create(patient_params)
+      redirect_to patient
     end
 
-    def doctor_params
-      params.require(:doctor).permit(:name, :department)
+    def patient_params
+      params.require(:patient).permit(:name, :age)
     end
 
 end
